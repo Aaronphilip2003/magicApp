dc88435b463118ecf5fda006e0132e1e45e192e2
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-01-10 23:36:25 +0530

    finalApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+
+  const [count,setCount]=useState(0);
+  const [youtubeURL, setYoutubeURL] = useState('');
+
+  const addButton=()=>{
+    setCount(count+1)
+  }
+
+  const subButton=()=>{
+    setCount(count-1)
+  }
+
+  const magBut=()=>{
+    setYoutubeURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+  }
+
+  if (youtubeURL) {
+    window.location.href = youtubeURL;
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="mx-auto my-auto text-center">
+      <div class="bg-gray-200 p-4">
+        {count}
+      </div>
     </div>
+    <div class="h-64 flex items-center justify-center">
+  <div class="flex justify-between text-center">
+    <button className="btn btn-primary mx-2 font-bold" onClick={addButton}>+</button>
+    <button className="btn btn-secondary mx-2 font-bold" onClick={subButton}>-</button>
+    <br/>
+    <button className="btn btn-secondary mx-2 font-bold" onClick={magBut}>Magic Button</button>
+  </div>
+</div>
+
+
+  </div>
+  
   );
 }
 
